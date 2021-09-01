74c3c8e035c74984297a53be54177a4db842f18a
icarovie <icarovieirra@gmail.com>
2021-09-01 01:56:04 -0300

    estilo

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React , { useState } from "react";
 import "./App.css";
 
 import { Typography, Container, Grid } from "@material-ui/core";
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import BalancoPatrimonial from "./components/BalancoPatrimonial/index";
 
 const App = () => {
-  const title = "Trabalho de Administração Financeira";
+  const title = "Analise Balanço Patrimonial";
+  const college = "Universidade Federal De Minas Gerais"
+  const course = "CAD167 - Administração Financeira"
+
+  const [renderBP, setRenderBP] = useState(false)
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+  }));
+  
 
   // mockado por enquanto
   const props = {
@@ -14,15 +29,26 @@ const App = () => {
     passivos: [],
   };
 
+  const classes = useStyles();
+
   return (
     <div className="App">
       <Grid container>
         <Grid item xs={12}>
           <Container spacing={1}>
+            <Typography variant="h6" component="h6" gutterBottom>
+              {college}<br/>
+              {course}<br/>
+            </Typography>
             <Typography variant="h3" component="h4" gutterBottom>
               {title}
             </Typography>
-            <BalancoPatrimonial {...props} />
+            <div className={classes.root}>
+              <Button variant="contained" color="secondary" onClick={() => { setRenderBP(false) }}>Inicio</Button>
+              <Button variant="contained" color="secondary">Sobre</Button>
+              <Button variant="contained" color="secondary" onClick={() => { setRenderBP(true) }}>Montar Balanço Patrimonial</Button>
+            </div>
+            { renderBP && <BalancoPatrimonial {...props} />}
           </Container>
         </Grid>
       </Grid>
